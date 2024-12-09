76de63013a6ee4666765917693c9345307f454ff
David Brodrick <128331752+davedevcodes@users.noreply.github.com>
2024-12-09 13:43:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ const observer = new IntersectionObserver((entries) => {
 
 // Observe the target element
 observer.observe(slideInElement);
+
+
+
+// Wait until all page content is fully loaded
+window.onload = function () {
+    // Hide the loading screen
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.opacity = '0'; // Fade out effect
+    loadingScreen.style.transition = 'opacity 0.5s ease';
+
+    // Wait for the fade-out effect to complete before removing the element
+    setTimeout(() => {
+        loadingScreen.style.display = 'none';
+        
+        // Show the main content
+        const mainContent = document
